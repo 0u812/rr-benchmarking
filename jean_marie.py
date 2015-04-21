902ca5e93fde90698437409f72fc16d005576ab8
0u812 <0u812@github.com>
2015-04-21 15:23:46 -0700

    update roadrunne jean_marie bench

--- jean_marie.py
+++ jean_marie.py
@@ -7,11 +7,24 @@ Created on Tue Apr 21 12:47:12 2015
 
 import roadrunner as rr
 import tellurium as te
+import csv
+import time
+from sys import stderr, stdout
+
+csvwriter = csv.writer(stdout, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+csvwriter.writerow([rr.getVersionStr()])
+csvwriter.writerow(['Name', 'Run_time'])
 
 r = rr.RoadRunner(r'C:\Users\phantom\Documents\devel\src\rr-benchmarking\jean_marie\Jean_Marie_AMPA16_RobHow_v6.xml')
 
-print(r.rv())
+#print(r.rv())
 
-print te.sbmlToAntimony(r.getCurrentSBML())
-m = r.simulate(0, 5000000, 10000)
-r.plot()
+name = 'JeanMarie'
+for n in range(3):
+    #print te.sbmlToAntimony(r.getCurrentSBML())
+    startTime = time.time()
+    m = r.simulate(0, 5000000, 10000)
+    #r.plot()
+    endTime = time.time()
+    csvwriter.writerow([name, endTime-startTime])
